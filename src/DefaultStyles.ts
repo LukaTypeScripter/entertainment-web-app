853d7a2b8e2fd442ec40c76d4ce93ec064f37504
luka shinjiakshvili <lukashinjikashvili85@gmail.com>
2023-06-20 21:38:51 +0400

    default styles

---
+++ src/DefaultStyles.ts
@@ -0,0 +1,12 @@
+
+
+
+export const DefStyles = {
+    MainColors:{
+       Red:"#FC4747",
+        DarkBlue:"#10141E",
+        GreyishBlue:"#5A698F",
+        SemiDarkBlue:'#161D2F',
+        White:"#FFFFFF"
+    }
+}
