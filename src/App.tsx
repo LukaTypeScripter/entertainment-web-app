1ab27bd50a8bf57e9b55df036997f56e258c625f
luka shinjiakshvili <lukashinjikashvili85@gmail.com>
2023-06-23 18:44:47 +0400

    added sme routes

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,31 @@ import { BrowserRouter as Router, Routes, Route, Outlet } from "react-router-dom
 import SignIn from './scenes/SignIn/SignIn';
 import SignUp from './scenes/signUp/SignUp';
 import Home from './scenes/Home/Home';
+import Movies from './scenes/movies/Movies';
+import Navigation from './scenes/Home/Navigation/Navigation';
+import TvSeries from './scenes/Tvseries/TvSeries';
+import BookMarked from './Bookmarked/BookMarked';
+import { MovieAppMain } from './scenes/Home/styles/homeStyles';
 
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <MovieAppMain>
 
 <Router>
+<Navigation/>
         <Routes>
-          <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/movies" element={<Movies />} />
+        <Route path="/series" element={<TvSeries />} />
+        <Route path="/bookmarked" element={<BookMarked />} />
           <Route path="/signin" element={<SignIn />} />
           <Route path="/signup" element={<SignUp />} />
         </Routes>
       </Router>
-    </>
+    </MovieAppMain>
   )
 }
 
