b959cda542e8c770a8ad304e4ea40f85e44023c5
luka shinjiakshvili <lukashinjikashvili85@gmail.com>
2023-06-24 14:19:29 +0400

    changed som issues

--- src/scenes/Home/Home.tsx
+++ src/scenes/Home/Home.tsx
@@ -103,16 +103,7 @@ function Home() {
                       // JSX for non-bookmarked movie
                       <>
                         <VideoBookMark onClick={() => toggleBookmark(index)}>
-                          <svg
-                            width="12"
-                            height="14"
-                            xmlns="http://www.w3.org/2000/svg"
-                          >
-                            <path
-                              d="M10.61 0c.14 0 .273.028.4.083a1.03 1.03 0 0 1 .657.953v11.928a1.03 1.03 0 0 1-.656.953c-.116.05-.25.074-.402.074-.291 0-.543-.099-.756-.296L5.833 9.77l-4.02 3.924c-.218.203-.47.305-.756.305a.995.995 0 0 1-.4-.083A1.03 1.03 0 0 1 0 12.964V1.036A1.03 1.03 0 0 1 .656.083.995.995 0 0 1 1.057 0h9.552Z"
-                              fill="#FFF"
-                            />
-                          </svg>
+                        <svg width="12" height="14" xmlns="http://www.w3.org/2000/svg"><path d="m10.518.75.399 12.214-5.084-4.24-4.535 4.426L.75 1.036l9.768-.285Z" stroke="#FFF" stroke-width="1.5" fill="none"/></svg>
                         </VideoBookMark>
                       </>
                     )}
@@ -163,11 +154,11 @@ function Home() {
             <VideosTitle>Bookmarked Tv series</VideosTitle>
           )}
           <VideosList>
-            {trendingMovies.map((movie, index) => (
+            {trendingMovies.filter((movie) => movie.isBookmarked).map((movie, index) => (
               <>
-                {!movie.isTrending && (
+                {movie.category === "TV Series"&& (
                   <VideosItem key={index}>
-                    <RecomendedVideo>
+                    <RecomendedVideo >
                       <VIdeoCard
                         style={{
                           backgroundImage: `url(${movie.thumbnail.regular.small})`,
@@ -190,16 +181,8 @@ function Home() {
                       // JSX for non-bookmarked movie
                       <>
                         <VideoBookMark onClick={() => toggleBookmark(index)}>
-                          <svg
-                            width="12"
-                            height="14"
-                            xmlns="http://www.w3.org/2000/svg"
-                          >
-                            <path
-                              d="M10.61 0c.14 0 .273.028.4.083a1.03 1.03 0 0 1 .657.953v11.928a1.03 1.03 0 0 1-.656.953c-.116.05-.25.074-.402.074-.291 0-.543-.099-.756-.296L5.833 9.77l-4.02 3.924c-.218.203-.47.305-.756.305a.995.995 0 0 1-.4-.083A1.03 1.03 0 0 1 0 12.964V1.036A1.03 1.03 0 0 1 .656.083.995.995 0 0 1 1.057 0h9.552Z"
-                              fill="#FFF"
-                            />
-                          </svg>
+                        <svg width="12" height="14" xmlns="http://www.w3.org/2000/svg"><path d="m10.518.75.399 12.214-5.084-4.24-4.535 4.426L.75 1.036l9.768-.285Z" stroke="#FFF" stroke-width="1.5" fill="none"/></svg>
+                          
                         </VideoBookMark>
                       </>
                     )}
