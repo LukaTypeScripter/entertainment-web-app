304f0735303c7a4f0dc2c31eeaf624172d1b9ba4
luka shinjiakshvili <lukashinjikashvili85@gmail.com>
2023-06-24 14:19:17 +0400

    added toggle bookmark

--- src/context/AppContext.tsx
+++ src/context/AppContext.tsx
@@ -44,17 +44,30 @@ interface Movie {
   }) => {
     const [trendingMovies ,setTrendingMovies ] = useState<Movie[] | undefined>(data)
     const toggleBookmark = (index: number) => {
-        setTrendingMovies((prevMovies) => {
-          const updatedMovies = prevMovies ? [...prevMovies] : [];
-          if (updatedMovies[index]) {
-            updatedMovies[index].isBookmarked = !updatedMovies[index].isBookmarked;
-            console.log(updatedMovies[index].isBookmarked)
-          }
-          
-          return updatedMovies;
-        });
-      };
+      setTrendingMovies((prevMovies: Movie[] | undefined) => {
+        if (!prevMovies) {
+          return [];
+        }
     
+        const updatedMovies = [...prevMovies];
+        if (updatedMovies[index]) {
+          updatedMovies[index].isBookmarked = !updatedMovies[index].isBookmarked;
+        }
+    
+        const bookmarkedMovies = updatedMovies.filter((movie) => movie.isBookmarked);
+        const unbookmarkedMovies = updatedMovies.filter((movie) => !movie.isBookmarked);
+    
+        // Remove the current movie from the bookmarkedMovies and unbookmarkedMovies arrays
+        const filteredBookmarkedMovies = bookmarkedMovies.filter((movie) => movie !== updatedMovies[index]);
+        const filteredUnbookmarkedMovies = unbookmarkedMovies.filter((movie) => movie !== updatedMovies[index]);
+    
+        if (updatedMovies[index]?.isBookmarked) {
+          return [...filteredUnbookmarkedMovies, updatedMovies[index], ...filteredBookmarkedMovies];
+        } else {
+          return [...filteredBookmarkedMovies, ...filteredUnbookmarkedMovies];
+        }
+      });
+    };
   
     
     const value: AppContextValue = {
